e2ec98d38e7921c8facb7b79fec2809d11c9872e
Zinmoeag <111127377+Zinmoeag@users.noreply.github.com>
2025-10-01 11:20:19 +0630

    feat(client): implement client query

--- src/client.ts
+++ src/client.ts
@@ -1,26 +1,26 @@
 import "dotenv/config";
-import {Client} from "@modelcontextprotocol/sdk/client/index.js";
-import {StdioClientTransport} from "@modelcontextprotocol/sdk/client/stdio.js";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
 import { confirm, input, select } from "@inquirer/prompts"
-import { Prompt, PromptMessage, Tool } from "@modelcontextprotocol/sdk/types";
-import { generateText } from "ai";
+import { CreateMessageRequestSchema, Prompt, PromptMessage, Tool } from "@modelcontextprotocol/sdk/types";
+import { ToolSet, generateText, tool as aiTool, jsonSchema } from "ai";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 
 
 
 const mcp = new Client({
-    name: "demo-client",
-    version: "1.0.0"
+  name: "demo-client",
+  version: "1.0.0"
 }, {
-    capabilities: {
-        sampling: {}
-    }
+  capabilities: {
+    sampling: {}
+  }
 });
 
 const transport = new StdioClientTransport({
-    command: "node",
-    args: ["dist/index.js"],
-    stderr: "ignore",
+  command: "node",
+  args: ["dist/index.js"],
+  stderr: "ignore",
 })
 
 const google = createGoogleGenerativeAI({
@@ -28,10 +28,10 @@ const google = createGoogleGenerativeAI({
 });
 
 const main = async () => {
-    console.log("starting client");
-    await mcp.connect(transport);
-    console.log("client connected");
-    const [{ tools }, { resources}, {resourceTemplates}, {prompts}] =
+  console.log("starting client");
+  await mcp.connect(transport);
+  console.log("client connected");
+  const [{ tools }, { resources }, { resourceTemplates }, { prompts }] =
     await Promise.all([
       mcp.listTools(),
       mcp.listResources(),
@@ -39,77 +39,99 @@ const main = async () => {
       mcp.listPrompts(),
     ])
 
+    mcp.setRequestHandler(CreateMessageRequestSchema, async request => {
+      const texts: string[] = []
+      console.log(request, "request")
+      for (const message of request.params.messages) {
+        const text = await handleServerMessagePrompt(message)
+        if (text != null) texts.push(text)
+      }
+  
+      return {
+        role: "user",
+        model: "gemini-2.0-flash",
+        stopReason: "endTurn",
+        content: {
+          type: "text",
+          text: texts.join("\n"),
+        },
+      }
+    })
 
-    while (true) {
-        const option = await select({
-            message: "What do you want to do?",
-            choices: ["Query", "Tools", "Resources", "Prompts"]
+
+  while (true) {
+    const option = await select({
+      message: "What do you want to do?",
+      choices: ["Query", "Tools", "Resources", "Prompts"]
+    })
+
+    switch (option) {
+      case "Tools":
+        const toolName = await select({
+          message: "Select a tool",
+          choices: tools.map((tool) => ({
+            name: tool.annotations?.title || tool.name,
+            value: tool.name,
+            description: tool.description,
+          })),
         })
+        const tool = tools.find(t => t.name === toolName)
+        if (tool == null) {
+          console.error("Tool not found.")
+        } else {
+          await handleTool(tool)
+        }
+        break;
+      case "Resources":
+        const avaiableResources = [
+          ...resources.map((resource) => ({
+            name: resource.name,
+            value: resource.uri,
+            description: resource.description,
+          })),
+          ...resourceTemplates.map((template) => ({
+            name: template.name,
+            value: template.uri,
+            description: template.description,
+          })),
+        ];
 
-        switch (option) {
-            case "Tools":
-                const toolName = await select({
-                message: "Select a tool",
-                choices: tools.map((tool) => ({
-                    name: tool.annotations?.title || tool.name,
-                    value: tool.name,
-                    description: tool.description,
-                })),
-                })
-                const tool = tools.find(t => t.name === toolName)
-                if (tool == null) {
-                console.error("Tool not found.")
-                } else {
-                  await handleTool(tool)
-                }
-                break;
-            case "Resources":
-                const avaiableResources = [
-                        ...resources.map((resource) => ({
-                            name: resource.name,
-                            value: resource.uri,
-                            description: resource.description,
-                        })),
-                        ...resourceTemplates.map((template) => ({
-                            name: template.name,
-                            value: template.uri,
-                            description: template.description,
-                        })),
-                    ];
-                
-                console.log(avaiableResources, "avaiableResources ====");
+        console.log(avaiableResources, "avaiableResources ====");
 
-                const resourceUri = await select({
-                    message: "Select a resource",
-                    choices: avaiableResources
-                })
-                const uri = resources.find(r => r.uri === resourceUri)?.uri ?? resourceTemplates.find(r => r.uriTemplate === resourceUri)?.uriTemplate;
-                console.log(uri, "uri");
+        const resourceUri = await select({
+          message: "Select a resource",
+          choices: avaiableResources
+        })
+        const uri = resources.find(r => r.uri === resourceUri)?.uri ?? resourceTemplates.find(r => r.uriTemplate === resourceUri)?.uriTemplate;
+        console.log(uri, "uri");
 
-                if (uri == null) {
-                    console.error("Resource not found.")
-                } else {
-                    await handleResource(uri);
-                }
-                break;
-            case "Prompts":
-                const promptName = await select({
-                  message: "Select a prompt",
-                  choices: prompts.map(prompt => ({
-                    name: prompt.name,
-                    value: prompt.name,
-                    description: prompt.description,
-                  })),
-                })
-                const prompt = prompts.find(p => p.name === promptName)
-                if (prompt == null) {
-                  console.error("Prompt not found.")
-                } else {
-                  await handlePrompt(prompt)
-                }
-                break
-                }
+        if (uri == null) {
+          console.error("Resource not found.")
+        } else {
+          await handleResource(uri);
+        }
+        break;
+      case "Prompts":
+        const promptName = await select({
+          message: "Select a prompt",
+          choices: prompts.map(prompt => ({
+            name: prompt.name,
+            value: prompt.name,
+            description: prompt.description,
+          })),
+        })
+        const prompt = prompts.find(p => p.name === promptName)
+        if (prompt == null) {
+          console.error("Prompt not found.")
+        } else {
+          await handlePrompt(prompt)
+        }
+        break
+      case "Query":
+        await handleQuery(tools)
+        break
     }
+  }
 }
 
 async function handleTool(tool: Tool) {
@@ -170,22 +192,62 @@ async function handlePrompt(prompt: Prompt) {
 }
 
 async function handleServerMessagePrompt(message: PromptMessage) {
-  if(message.content.type !== "text") return;
+  if (message.content.type !== "text") return;
 
   console.log("Message from prompt:", message.content.text);
-  
+
   const run = await confirm({
     message: "Do you want to run this?",
     default: true
   });
 
-  if(!run) return;
+  if (!run) return;
 
-  const {text} = await generateText({
+  const { text } = await generateText({
     model: google("gemini-2.0-flash"),
     prompt: message.content.text,
   })
   return text;
+}
+
+async function handleQuery(tools: Tool[]) {
+  const query = await input({
+    message: "Enter your query:",
+  });
+
+  const { text, toolResults } = await generateText({
+    model: google("gemini-2.0-flash"),
+    prompt: query,
+    tools: {
+      create_user: {
+        description: "Create a user",
+        inputSchema: jsonSchema(tools[0].inputSchema),
+        execute: async (args: any) => {
+          const res = await mcp.callTool({
+            name: tools[0].name,
+            arguments: args,
+          })
+          console.log(res, "create user")
+          return res;
+
+        },
+      },
+      create_random_user: {
+        description: "Create a random user",
+        inputSchema: jsonSchema(tools[1].inputSchema),
+        execute: async (args: any) => {
+          const res = await mcp.callTool({
+            name: tools[1].name,
+            arguments: args,
+          })
+          console.log(res, "create random user")
+          return res;
+        },
+      },
+    },
+  })
+  console.log(text || (toolResults[0] as any)?.result?.content[0]?.text || "No text generated.");
+  console.log(text, "text");
 }
 
 main();
